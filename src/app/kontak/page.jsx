d35cd92771eabffbf73c581ffe09a192e2ae9bf4
junialdy <junialdyp0629@gmail.com>
2024-06-11 07:57:49 +0700

    Add Single Product Page

--- src/app/kontak/page.jsx
+++ src/app/kontak/page.jsx
@@ -1,5 +1,13 @@
+import Slider from "@/components/slider/Slider";
+
 const KontakPage = () => {
-  return <div>KontakPage</div>;
+  return (
+    <div>
+      <h1>KontakPage</h1>
+      <hr />
+      <Slider />
+    </div>
+  );
 };
 
 export default KontakPage;
